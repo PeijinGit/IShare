7746c7e6f7399596f6e7e30c92fc4dfec85db375
Yue Wang <yue.wang@plexure.com>
2021-01-07 09:44:12 +1300

    bussiness proj

---
+++ IShare/API/Controllers/EventsController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Business.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EventsController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Event> Get()
+        {
+            return new List<Event>
+            {
+                new Event{ Id = 42 , Name = "Friday Party" },
+                new Event{ Id = 99 , Name = "New Year meeting" }
+            };
+        }
+    }
+}
---
+++ IShare/Business/Models/Event.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Models
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
